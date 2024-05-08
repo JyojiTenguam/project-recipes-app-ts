af94c1dda99aab9915e3c0d73f0370cdf0b8556e
Daniel Frade <daniofj@gmail.com>
2024-05-08 06:29:36 -0300

    daniel-frade-requisito-4-parcial-faltando-os-testes-de-45%-de-cobertura

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const validateEmail = (inputEmail: string) => {
+    return inputEmail.includes('@') && inputEmail.includes('.');
+  };
+
+  const isValid = validateEmail(email) && password.length > 6;
+
   return (
     <div>
       <form>
@@ -8,16 +17,20 @@ function Login() {
           type="email"
           data-testid="email-input"
           placeholder="Email"
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
         />
         <input
           type="password"
           data-testid="password-input"
           placeholder="Senha"
+          value={ password }
+          onChange={ (e) => setPassword(e.target.value) }
         />
         <button
           type="submit"
           data-testid="login-submit-btn"
-          disabled
+          disabled={ !isValid }
         >
           Entrar
         </button>
