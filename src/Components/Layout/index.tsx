44d67bed0061c0b9dcf84a73f1fa54ea2c522d58
Junior <juniorwender@yahoo.o.com.br>
2024-05-09 10:24:29 -0300

    Junior corrigindo req 43

--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../Header';
 import Footer from '../Footer';
+import DoneRecipes from '../DoneRecipes';
 
 function Layout() {
   return (
     <>
       <Header />
       <Footer />
+      <DoneRecipes />
     </>
   );
 }
