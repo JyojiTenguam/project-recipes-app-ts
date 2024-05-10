a3070e8301aeb3f8d11717cc0e39ba7afe48094c
Daniel Frade <daniofj@gmail.com>
2024-05-10 19:26:23 -0300

    código da tela de login finalizado, faltando apenas os testes de cobertura (reqs 2 a 6)

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,36 +1,50 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-  const validateEmail = (inputEmail: string) => {
-    return inputEmail.includes('@') && inputEmail.includes('.');
+  const isValidEmail = (emailToCheck: string) => {
+    return /\S+@\S+\.\S+/.test(emailToCheck);
   };
 
-  const isValid = validateEmail(email) && password.length > 6;
+  const isFormValid = () => {
+    return isValidEmail(email) && password.length > 6;
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (isFormValid()) {
+      localStorage.setItem('user', JSON.stringify({ email }));
+      navigate('/meals');
+    }
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <input
           type="email"
           data-testid="email-input"
           placeholder="Email"
           value={ email }
-          onChange={ (e) => setEmail(e.target.value) }
+          onChange={ (e: React.
+            ChangeEvent<HTMLInputElement>) => setEmail(e.target.value) }
         />
         <input
           type="password"
           data-testid="password-input"
           placeholder="Senha"
           value={ password }
-          onChange={ (e) => setPassword(e.target.value) }
+          onChange={ (e: React.
+            ChangeEvent<HTMLInputElement>) => setPassword(e.target.value) }
         />
         <button
           type="submit"
           data-testid="login-submit-btn"
-          disabled={ !isValid }
+          disabled={ !isFormValid() }
         >
           Entrar
         </button>
