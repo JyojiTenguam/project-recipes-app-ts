b8f1dd19e1d8b6aab967a49be6e7005169ddd804
Daniel Frade <daniofj@gmail.com>
2024-05-14 19:36:47 -0300

    requisito 56 passando nos testes

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Profile() {
   return (
-    <h1>Profile h1</h1>
+    <div>
+      <h1>Profile</h1>
+      <p data-testid="profile-email">email@example.com</p>
+      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
+    </div>
   );
 }
 
