71be28cf29e598b7ee0ef7cc25d4c762e2997639
Daniel Frade <daniofj@gmail.com>
2024-05-16 01:16:14 -0300

    requisito 59 sem os testes e requisito 60

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
+  const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem('user') || '{}');
 
   return (
     <div>
-      <h1 data-testid="profile-email">{user.email || 'No email'}</h1>
-      <button data-testid="profile-done-btn">
+      <h1 data-testid="profile-email">{user.email}</h1>
+      <button
+        data-testid="profile-done-btn"
+        onClick={ () => navigate('/done-recipes') }
+      >
         Done Recipes
       </button>
-      <button data-testid="profile-favorite-btn">
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={ () => navigate('/favorite-recipes') }
+      >
         Favorite Recipes
       </button>
       <button data-testid="profile-logout-btn">
