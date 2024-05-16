fe0196ce8aecc23faf3838067b18dfc16028b7f3
Junior <juniorwender@yahoo.o.com.br>
2024-05-16 16:58:19 -0300

    Junior test 85% FavoriteRecipes

---
+++ src/tests/FavoriteRecipes.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import FavoriteRecipes from '../Components/FavoriteRecipes';
+
+describe('FavoriteRecipes test - Junior', () => {
+  const filterAllBtn = 'filter-by-all-btn';
+  const filterMealBtn = 'filter-by-meal-btn';
+  const filterDrinkBtn = 'filter-by-drink-btn';
+
+  it('Verifica se o botão "Todos" está presente e possui o data-testid correto', () => {
+    render(<FavoriteRecipes />);
+    const allButton = screen.getByTestId(filterAllBtn);
+    expect(allButton).toBeInTheDocument();
+    expect(allButton).toHaveAttribute('data-testid', filterAllBtn);
+  });
+
+  it('Verifica se o botão Meals está presente na tela', () => {
+    render(<FavoriteRecipes />);
+    const buttonMeals = screen.getByRole('button', { name: /meals/i });
+    expect(buttonMeals).toBeInTheDocument();
+  });
+
+  it('Verifica se renderiza os botões de filtro.', () => {
+    render(<FavoriteRecipes />);
+    expect(screen.getByTestId(filterAllBtn)).toBeInTheDocument();
+    expect(screen.getByTestId(filterMealBtn)).toBeInTheDocument();
+    expect(screen.getByTestId(filterDrinkBtn)).toBeInTheDocument();
+  });
+
+  it('Verifica se filtra as receitas pelo tipo.', () => {
+    render(<FavoriteRecipes />);
+    fireEvent.click(screen.getByTestId(filterMealBtn));
+    fireEvent.click(screen.getByTestId(filterDrinkBtn));
+    fireEvent.click(screen.getByTestId(filterAllBtn));
+  });
+
+  it('Verifica se ao clicar em "Desfavoritar" remove a receita da lista de favoritos.', () => {
+    render(<FavoriteRecipes />);
+    const favoriteButton = screen.getByTestId('0-horizontal-favorite-btn');
+
+    fireEvent.click(favoriteButton);
+    expect(screen.queryByTestId('0-horizontal-image')).toBeNull();
+  });
+
+  it('Verifica se ao clicar em "Compartilhar" exibe a mensagem de link copiado.', async () => {
+    render(<FavoriteRecipes />);
+    const shareButton = screen.getByTestId('0-horizontal-share-btn');
+
+    fireEvent.click(shareButton);
+    const copiedMessage = await screen.findByText('Link copied!');
+    expect(copiedMessage).toBeInTheDocument();
+  });
+});
